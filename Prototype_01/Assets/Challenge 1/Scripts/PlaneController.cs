fa5abe2c3a88f93a38ddd457aa9669ddd9c82459
JHadley98 <james.hadley1@live.co.uk>
2024-08-03 16:28:06 +0100

    Debugged & fixed plane controls, added rotation to propeller
    Fixed issue with planes movement updating the translate to use .forward instead of .back to allow the plane to go forward. Updated plane tilt rotation use vertical input to allow the player to control the planes up and down direction. Created an object reference for the propeller on the player controller class for the plane rotating the propeller on the Z axis.

---
+++ Prototype_01/Assets/Challenge 1/Scripts/PlaneController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    [Header("Plane Controls")]
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float rotationSpeed;
+    [SerializeField]
+    private float verticalInput;
+
+    [Header("Propeller Variables")]
+    [SerializeField]
+    GameObject propeller;
+    [SerializeField]
+    private float propellerSpeed;
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
+
+        // Rotate propeller on plane
+        propeller.transform.Rotate(Vector3.forward * propellerSpeed * Time.deltaTime);
+    }
+}

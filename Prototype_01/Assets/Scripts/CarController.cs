08c1f8e95ca475133f26d40cf0696d400453b1b0
JHadley98 <james.hadley1@live.co.uk>
2024-08-04 17:02:41 +0100

    Updated Car Controller
    Rearranged movement and rotation logic to call Time.deltaTime yet to provide smooth overall movement.

--- Prototype_01/Assets/Scripts/CarController.cs
+++ Prototype_01/Assets/Scripts/CarController.cs
@@ -23,8 +23,8 @@ public class CarController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // Moves the car forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * verticalInput);
+        transform.Translate(Vector3.forward * moveSpeed * verticalInput * Time.deltaTime);
         // Rotates the car based on horizontal input
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
